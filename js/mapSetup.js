393fb0fb675f9537ed7d4981f64ff634f5b61c94
Jacob Reid <reidwjacob@gmail.com>
2025-08-04 09:09:27 -0600

    Added logo and favicon and dark mode

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -1,8 +1,10 @@
 export function createMap() {
   const map = L.map("map").setView([39.5, -98.35], 4); // Center on US
+
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "© OpenStreetMap",
   }).addTo(map);
+
   return map;
 }
